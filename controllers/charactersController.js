514a9c1a2a36b8bc39f44f9a776e2cb0327b89eb
zotacy <tacyzacharyo@johndeere.com>
2020-12-09 10:25:11 -0500

    Backend MVC Setup-inital

---
+++ controllers/charactersController.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const CharacterModel = require("../models").Character;
+
+// GET CHARACTER DATA
+
+
+module.exports = router;
